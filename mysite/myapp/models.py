f67ddd297982eedaef0654766cd6bbd3a3ae7ea5
Yaroslav <yrmn10@gmail.com>
2023-10-16 21:05:58 +0300

    hw3

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Topic(models.Model):
     title = models.CharField(max_length=64)
-    descripion = models.CharField(max_length=255)
-    subscribers = models.ManyToManyField(User, related_name='topics')
+    description = models.CharField(max_length=255, default='')
+    subscribers_new = models.ManyToManyField(User, through='Preference')
 
     def __str__(self):
         return self.title
@@ -16,18 +16,24 @@ class Article(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
-    topics = models.ManyToManyField(Topic, related_name='topics')
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topics = models.ManyToManyField(Topic)
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    message = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    article = models.ForeignKey(Article, on_delete=models.DO_NOTHING, related_name='comments')
+    message = models.CharField(max_length=512)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, models.DO_NOTHING)
 
     def __str__(self):
         return self.message
+
+
+class Preference(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    is_notified = models.BooleanField(default=False)
